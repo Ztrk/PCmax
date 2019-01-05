5e14810929d6d85cdc3d51caf3c9425bf8784f5f
Marcin Zatorski <mar3211233@gmail.com>
2019-01-05 02:11:31 +0100

    Optimization

--- PCmax/LPT.cpp
+++ PCmax/LPT.cpp
@@ -1,18 +1,23 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
+#include <queue>
 #include "LPT.h"
 using namespace std;
 
 
 int list_algorithm(int processors, const vector<int> &jobs) {
-	vector<int> processor_time(processors, 0);
+	priority_queue<int, vector<int>, greater<int>> processor_time(greater<int>(), vector<int>(processors, 0));
 
+	int result = 0;
 	for (unsigned i = 0; i < jobs.size(); ++i) {
-		*min_element(processor_time.begin(), processor_time.end()) += jobs[i];
+		int min_proc = processor_time.top();
+		processor_time.pop();
+		processor_time.push(min_proc + jobs[i]);
+		result = max(result, min_proc + jobs[i]);
 	}
 
-	return *max_element(processor_time.begin(), processor_time.end());
+	return result;
 }	
 
 int lpt(int processors, vector<int> jobs) {
